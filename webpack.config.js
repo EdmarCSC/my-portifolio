d5e5e8960471a2df27c50b3c43c32f644fe4e906
Edmar Carlos <edmarcsc23@hotmail.com>
2022-08-04 21:21:48 -0300

    Started my portifólio

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+ 
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Meu portifólio',
+      filename: 'index.html',
+      template: 'index.html'
+    }),
+  ],
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
